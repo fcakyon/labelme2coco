8f9c5fd3755bddd200fbd8e3d1bf8573702a0662
Hannah Markfort <74815681+xCatalitY@users.noreply.github.com>
2024-04-19 09:23:07 +0000

    Included: If data is structured in subdirectories, the pathes are missing in the "file_name" attribute. Instead of subfolder_1/image.jpeg the filename contains only image.jpeg.
    New:
    Add category_id_start parameter to support starting category IDs from 1

--- labelme2coco/cli.py
+++ labelme2coco/cli.py
@@ -5,7 +5,7 @@ from labelme2coco import convert
 
 def app() -> None:
     """Cli app."""
-    fire.Fire(convert)
+    fire.Fire(lambda labelme_folder, export_dir="runs/labelme2coco/", train_split_rate=1, skip_labels=[], category_id_start=0: convert(labelme_folder, export_dir, train_split_rate, skip_labels, category_id_start))
 
 
 if __name__ == "__main__":
